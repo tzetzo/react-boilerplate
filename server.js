5cb74fd7ea73f6b2547991ea25520acbe244bcca
tzetzo <tzvetanmarinov@yahoo.com>
2019-07-19 01:20:45 +0300

    comments update

--- server.js
+++ server.js
@@ -4,9 +4,7 @@ const path = require("path");
 const port = process.env.PORT || 8080; //process.env.PORT is provided by heroku
 const app = express();
 
-console.log(__dirname);
-
-app.use(express.static(__dirname + "/build")); //provides a static website with bundle.js containing all JS code build by webpack
+app.use(express.static(__dirname + "/build")); //provides a static website under the build folder containing all JS code build by webpack
 
 app.get("*", (req, res) => {
   //sends the user index.html by default
