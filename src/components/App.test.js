b545d9a85122270261e486af176a66ebc549618f
tzetzo <tzvetanmarinov@yahoo.com>
2019-07-19 04:10:32 +0300

    folders included

---
+++ src/components/App.test.js
@@ -0,0 +1,11 @@
+//https://facebook.github.io/create-react-app/docs/running-tests
+//https://jestjs.io/docs/en/expect.html#content
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+
+it("renders without crashing", () => {
+  const div = document.createElement("div");
+  ReactDOM.render(<App />, div);
+  ReactDOM.unmountComponentAtNode(div);
+});
